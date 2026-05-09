3d18ec68e3b303bba6353d8db26590258935f2e3
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2026-05-09 14:35:01 -0700

    Recovered a good chunk of the files

---
+++ blinking-led/main/blinking-led.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+
+#include "driver/gpio.h"
+#include "soc/gpio_num.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+// This is defined in the menuconfig
+#define BLINK_GPIO CONFIG_BLINK_GPIO
+#define BLINK_DURATION CONFIG_BLINK_DURATION
+
+void app_main(void) {
+  gpio_reset_pin(BLINK_GPIO);
+  gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+  uint32_t state = 1;
+
+  for (;;) {
+    gpio_set_level(GPIO_NUM_2, state);
+
+    state = !state;
+
+    vTaskDelay(BLINK_DURATION / portTICK_PERIOD_MS);
+  }
+}
