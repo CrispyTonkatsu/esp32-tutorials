e4d8b84f622141a6c2f341c7dfed3860299505e5
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2026-06-22 20:43:03 -0700

    Completed the sensor startup in a cleaner format

--- dht11/main/dht11.c
+++ dht11/main/dht11.c
@@ -2,38 +2,72 @@
 #include "esp_log.h"
 #include "esp_timer.h"
 
+#include "freertos/projdefs.h"
 #include "rom/ets_sys.h"
 
 #include "driver/gpio.h"
 #include "hal/gpio_types.h"
 #include "soc/gpio_num.h"
-#include <stddef.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 #define SENSOR_PIN GPIO_NUM_27 // CONFIG_SENSOR_PIN
-#define POLL_RATE_MS 100       // CONFIG_POLL_RATE_MS
-#define TIMEOUT_MS 0.4         // CONFIG_TIMEOUT_MS
+#define POLL_RATE_MS 1000      // CONFIG_POLL_RATE_MS
 
 const static char *tag = "DHT11";
 
-/**
- * Processing the ack signal
- * @return True if the function succeeded, false if it timed out
- */
-bool process_ack_signal() {
-  ESP_ERROR_CHECK(gpio_set_direction(SENSOR_PIN, GPIO_MODE_INPUT));
+typedef struct {
+  int64_t duration;
+  bool timed_out;
+} WaitData;
 
+inline WaitData WaitData_make(int64_t duration, bool timed_out) {
+  const WaitData output = {
+      .duration = duration,
+      .timed_out = timed_out,
+  };
+  return output;
+}
+
+WaitData wait_for_state(gpio_num_t pin, int state, int64_t timeout) {
   const int64_t start_time = esp_timer_get_time();
-  while (gpio_get_level(SENSOR_PIN) == 1) {
+
+  while (gpio_get_level(SENSOR_PIN) != state) {
     const int64_t time = esp_timer_get_time();
 
-    if (time - start_time > 400) {
-      return false;
+    if (time - start_time > timeout) {
+      return WaitData_make(time - start_time, true);
     }
   }
 
-  // TODO: Left off here: Implementing the check for the 80 microsecond low
+  return WaitData_make(esp_timer_get_time() - start_time, false);
+}
 
+/**
+ * Starting the data stream from the sensor
+ * @return True if the function succeeded, false if it timed out
+ */
+bool start_sensor() {
+  ESP_ERROR_CHECK(gpio_set_level(SENSOR_PIN, 0));
+  ets_delay_us(18000);
 
+  ESP_ERROR_CHECK(gpio_set_direction(SENSOR_PIN, GPIO_MODE_INPUT));
+
+  if (wait_for_state(SENSOR_PIN, 0, 40).timed_out) {
+    ESP_LOGE(tag, "Timeout on wait for device");
+    return false;
+  }
+
+  if (wait_for_state(SENSOR_PIN, 1, 80).timed_out) {
+    ESP_LOGE(tag, "Timeout on device low");
+    return false;
+  }
+
+  if (wait_for_state(SENSOR_PIN, 0, 80).timed_out) {
+    ESP_LOGE(tag, "Timeout on device high");
+    return false;
+  }
 
   return true;
 }
@@ -48,24 +82,15 @@ void app_main(void) {
 
   ESP_ERROR_CHECK(gpio_set_level(SENSOR_PIN, 1));
 
-  for (int iter_count = 0;; iter_count++) {
+  for (;;) {
     ESP_ERROR_CHECK(gpio_set_direction(SENSOR_PIN, GPIO_MODE_OUTPUT));
 
-    // Start signal
-    ESP_ERROR_CHECK(gpio_set_level(SENSOR_PIN, 0));
-    ets_delay_us(18000);
-
-    // Waiting for the ack signal
-    if (!process_ack_signal()) {
-      ESP_LOGE(tag, "Timeout on ack signal, iterations: %i", iter_count);
+    if (!start_sensor()) {
       return;
     }
 
-    // Reading the data
+    // TODO: Reading the data
 
-    // TODO: Remove this loop (this is just because we are doing one reading per
-    // program run now)
-    for (;;) {
-    }
+    vTaskDelay(pdMS_TO_TICKS(POLL_RATE_MS));
   }
 }
