ab3d8b5480d90aff44dbfcf9b98ed3c32781a41b
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2026-08-29 13:16:27 -0700

    Finished decoding the IR signal Now I will make it do media keys in my computer

--- ir-remote/main/ir-remote.c
+++ ir-remote/main/ir-remote.c
@@ -1,21 +1,22 @@
-#include "driver/rmt_types.h"
 #include "esp_err.h"
 #include "esp_log.h"
 
 #include "driver/rmt_rx.h"
+#include "driver/rmt_types.h"
+#include "hal/rmt_types.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/idf_additions.h"
 #include "freertos/projdefs.h"
-#include "hal/rmt_types.h"
 #include "portmacro.h"
-#include "time.h"
-#include <stddef.h>
 
 static const char *tag = "IR-Receiver";
 static rmt_channel_handle_t rx_channel_handle = NULL;
 static TaskHandle_t decode_ir_handle = NULL;
 static QueueHandle_t rx_queue_handle = NULL;
+
+static TaskHandle_t print_values_handle = NULL;
+static QueueHandle_t decoded_values_queue_handle = NULL;
 
 bool rx_recv_done_callback(rmt_channel_handle_t rx_chan,
                            const rmt_rx_done_event_data_t *edata,
@@ -25,6 +26,18 @@ bool rx_recv_done_callback(rmt_channel_handle_t rx_chan,
   xQueueSendFromISR(rx_queue_handle, edata, &pxHigherPriorityTaskWoken);
 
   return (pxHigherPriorityTaskWoken == pdTRUE);
+}
+
+bool enqueue_decoded_value(uint32_t value) {
+  const BaseType_t enqueue_result =
+      xQueueSend(decoded_values_queue_handle, &value, 0);
+
+  if (enqueue_result == errQUEUE_FULL) {
+    ESP_LOGW(tag, "The queue for decoded values is full");
+    return false;
+  }
+
+  return true;
 }
 
 void decode_ir(void *args) {
@@ -43,6 +56,7 @@ void decode_ir(void *args) {
 
     rmt_rx_done_event_data_t edata;
     xQueueReceive(rx_queue_handle, &edata, portMAX_DELAY);
+    input_code = 0;
 
     const rmt_symbol_word_t *leader_code = &edata.received_symbols[0];
     if (leader_code->duration0 < 8000 || leader_code->duration0 > 10000) {
@@ -57,24 +71,57 @@ void decode_ir(void *args) {
       }
 
       ESP_LOGI(tag, "Repeating previous code");
+      enqueue_decoded_value(input_code);
+
       continue;
     }
 
     ESP_LOGI(tag, "NEC Frame Start");
 
-    // for (size_t i = 1; i < edata.num_symbols; i++) {
-    //   const rmt_symbol_word_t *word = &edata.received_symbols[i];
-    //
-    //   // TODO: Left off here, decoding the NEC signal into bits
-    //
-    // }
+    bool failed = false;
 
+    for (size_t i = 1; i < edata.num_symbols; i++) {
+      const rmt_symbol_word_t *word = &edata.received_symbols[i];
+
+      ESP_LOGI(tag, "Values are: 0:%i, 1:%i", word->duration0, word->duration1);
+
+      if (word->duration0 < 400 || word->duration0 > 700) {
+        ESP_LOGW(tag, "The bit header is of invalid duration: %i",
+                 word->duration0);
+
+        failed = true;
+        break;
+      }
+
+      if (word->duration1 == 0) {
+        break;
+      }
+
+      input_code <<= 1;
+      input_code |= (word->duration1 > 1000) ? 1 : 0;
+    }
+
+    if (failed) {
+      continue;
+    }
+
+    enqueue_decoded_value(input_code);
     ESP_LOGI(tag, "NEC Frame End");
+  }
+}
+
+void print_values(void *args) {
+  while (true) {
+    uint32_t value;
+    xQueueReceive(decoded_values_queue_handle, &value, portMAX_DELAY);
+
+    ESP_LOGI(tag, "Input received: %i", value);
   }
 }
 
 void app_main(void) {
   rx_queue_handle = xQueueCreate(8, sizeof(void *));
+  decoded_values_queue_handle = xQueueCreate(8, sizeof(uint32_t));
 
   rmt_rx_channel_config_t rx_channel_config = {
       .gpio_num = GPIO_NUM_19,
@@ -93,5 +140,7 @@ void app_main(void) {
   ESP_ERROR_CHECK(rmt_enable(rx_channel_handle));
   rmt_rx_register_event_callbacks(rx_channel_handle, &callbacks, NULL);
 
-  xTaskCreate(decode_ir, "decode_ir", 4096, NULL, 2, &decode_ir_handle);
+  xTaskCreate(decode_ir, "decode_ir", 4096, NULL, 3, &decode_ir_handle);
+  xTaskCreate(print_values, "print_values", 4096, NULL, 1,
+              &print_values_handle);
 }
